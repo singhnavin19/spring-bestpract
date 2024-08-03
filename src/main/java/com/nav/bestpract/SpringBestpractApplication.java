8594d1b56a6cc6a5a6e57e61180ffd44d586e483
singhnavin19 <singhnavin19290@gmail.com>
2024-08-03 23:07:05 +0530

    Initial commit

---
+++ src/main/java/com/nav/bestpract/SpringBestpractApplication.java
@@ -0,0 +1,13 @@
+package com.nav.bestpract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBestpractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBestpractApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nav/bestpract/SpringBestpractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nav.bestpract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBestpractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
